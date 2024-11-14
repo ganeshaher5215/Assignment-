32269f3e8a81aca2509e8934bd921642f8d2682e
Ganesh Aher <ganeshaher5215@gmail.com>
2024-11-14 13:57:59 +0530

    Add files via upload

---
+++ exchan.cpp
@@ -0,0 +1,3 @@
+//creat a class to hold model no , screen size and the price . member functom include overladel insertion and extreactio poerator if more than four digit are are inter foer mopdel no if the screen size more than 17 or the price is negative then thron an intiger write a main function that inastatiate a telivision allow user to enter dta and display member if an eplection repless all daata member  
+#include<iostream>
+using namespace std;
